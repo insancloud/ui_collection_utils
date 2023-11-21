df9754d76a4c0ec6221607d283b6d20876456d4f
insancloud <insan5272@icloud.com>
2023-11-21 11:49:33 +0800

    Collection Utils

---
+++ android/app/src/main/kotlin/com/example/ui_collection/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ui_collection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('UI Colection Utils'),
+        ),
+        body: const Center(
+          child: Text(
+            'Hello, World!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
